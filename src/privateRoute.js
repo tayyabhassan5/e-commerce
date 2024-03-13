f3e9389a1b7c426697af8e2a9524a40f39726109
Tayyab Hassan <tayyab.hassan.rana@gmail.com>
2024-03-13 11:31:19 +0500

    merging in main

--- src/privateRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
-
-export const ProtectedRoute = ({ element: Element, ...rest }) => {
-  const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/, '$1');
-
-  return token ? <Route {...rest} element={<Element />} /> : <Navigate to="/login" />;
-};
