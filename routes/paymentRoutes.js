e86d58c7856fb3ef81d54ae6e4f6685d58d2f01b
tayyabhassan5 <tayyab.hassan.rana@gmail.com>
2024-03-13 15:23:42 +0500

    nx

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,9 @@
 const express=require('express');
-const {paymentOrder}=require('../controllers/paymentController')
+const {paymentOrder,paymentStripe}=require('../controllers/paymentController')
 
 const router=express.Router();
 
 router.get('/',paymentOrder);
+router.post('/stripe',paymentStripe)
 
 module.exports=router;
