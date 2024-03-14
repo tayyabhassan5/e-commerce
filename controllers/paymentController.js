49411a5891f901396ca07c0bf469dd24f4627e1f
tayyabhassan5 <tayyab.hassan.rana@gmail.com>
2024-03-14 15:04:18 +0500

    stripe backend

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,20 +1,46 @@
 const asyncHandler = require('express-async-handler');
-
+//const uuid=require('uuid');
+const stripe=require('stripe')('sk_test_51OltXJSByPnkUP3uU8khIPj6FrwWdSfmnkCYyqHzJdRZpJRRDylpjccUFNpT1C2NvIwD5olIsO0lNfV9yfPFAKXt00Y1taD43t')
+const { v4: uuid } = require('uuid');
 const paymentOrder = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Hello" });
 });
 
 const paymentStripe = asyncHandler(async (req, res) => {
     const { product, price } = req.body;
+    const idempotencyKey=uuid();
     res.status(200).json({
         message: "Hellsbo",
         product: product,
-        price: price
+        price: price,
+        uuid:idempotencyKey
+       
     });
     // const { product, price } = req.body;
-    // res.status(200).json(product);
+    // res.status(200).json(product)
     console.log("product", product);
-    console.log("proce", price)
+    console.log("proce", price);
+    console.log("uuid",idempotencyKey);
+
+    return stripe.customers.create({
+        email:TokenExpiredError,
+        source:Token.id,
+    }).then(customer=>{
+        stripe.charges.create({
+            amount:product.price*100,
+            currency:'usd',
+            customer:customer.id,
+            receipt_email:token.email,
+            description:`Purchase of ${product.name}`,
+            shipping:{
+                name:token.card.name,
+                address:{
+                    country:token.card.address_country
+                }
+            }
+        },{idempotencyKey})
+    }).then(result=>res.status(200).json(result))
+    .catch(err=>console.log(err))
 });
 module.exports = {
     paymentOrder,
